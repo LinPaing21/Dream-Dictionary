10bfb0e4d7058385931fa912413ead3830f46206
Nyan Lin Paing <117423022+LinPaing21@users.noreply.github.com>
2023-10-14 20:45:10 +0630

    Update header.vue

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,14 +1,5 @@
 <template>
     <header>
-        <!-- <nav class="navbar bg-primary">
-            <div class="container-fluid">
-                <span class="navbar-brand ms-3 mb-0 h1 fw-bold text-white"> အိပ်မက် အဘိဓာန်</span>
-                <ul class="navbar-nav">
-                    <li class="nav-item">ပင်မ စာမျက်နှာ</li>|
-                    <li class="nav-item">အက္ခရာစဥ်</li>
-                </ul>
-            </div>
-        </nav> -->
         <nav class="navbar navbar-expand-lg" style="background-color: #e3f2fd;">
             <div class="container-fluid">
                 <span class="navbar-brand ms-3 mb-0 h1 fw-bold"> အိပ်မက် အဘိဓာန်</span>
